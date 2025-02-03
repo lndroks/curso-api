8a152f819236c78a1a7e62f71bcdf91c74cf46d6
Leandro Salvador <personal.pajamas872@slmails.com>
2025-02-03 16:27:21 -0300

    Aula 01 - Modulo 3

---
+++ ScreenSound-API/Filtros/LinqFilter.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using ScreenSound_API.Modelos;
+
+namespace ScreenSound_API.Filtros;
+
+internal class LinqFilter
+{
+    public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+    {
+        var todosOsGenerosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();
+        foreach (var genero in todosOsGenerosMusicais)
+        {
+            Console.WriteLine($"- {genero}");
+        }
+    }
+}
